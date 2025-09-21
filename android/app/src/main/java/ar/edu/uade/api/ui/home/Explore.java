c59606ac933d8ea125d3cb3272eb99b6d5562025
Mariano <mariano_ef@live.com>
2025-09-20 22:17:19 -0300

    home(explore) - hardcoded data

---
+++ android/app/src/main/java/ar/edu/uade/api/ui/home/Explore.java
@@ -0,0 +1,15 @@
+package ar.edu.uade.api.ui.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ar.edu.uade.api.R;
+
+public class Explore extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.explore);
+    }
+}
