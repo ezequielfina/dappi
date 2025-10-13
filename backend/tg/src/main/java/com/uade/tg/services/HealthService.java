4c14e8c8e8d387d0d3b37326712a357c7fc8c9a6
ezequielfina <finaezeq@gmail.com>
2025-10-13 14:34:30 -0300

    bugfix

--- backend/tg/src/main/java/com/uade/tg/services/HealthService.java
+++ backend/tg/src/main/java/com/uade/tg/services/HealthService.java
@@ -4,7 +4,6 @@ import com.uade.tg.entities.Health;
 import com.uade.tg.exceptions.BusinessException;
 import com.uade.tg.repositories.HealthRep;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.engine.jdbc.spi.SqlExceptionHelper;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
