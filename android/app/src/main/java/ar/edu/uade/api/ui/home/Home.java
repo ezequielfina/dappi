d40ac78d5accec1795866ebcf1c125adb8770445
nicoredaelli <nicolasredaelli2@gmail.com>
2025-11-18 23:42:43 -0300

    fix: Agregar import faltante de Toast en Home.java

--- android/app/src/main/java/ar/edu/uade/api/ui/home/Home.java
+++ android/app/src/main/java/ar/edu/uade/api/ui/home/Home.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
